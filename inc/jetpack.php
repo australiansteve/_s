1ade3c0f21ade59df0e32587399e3c250615ad2c
Steve <steve@australiansteve.com>
2020-09-21 16:45:17 -0300

    Initial commit of Hamburger Cat, from _s

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -4,7 +4,7 @@
  *
  * @link https://jetpack.com/
  *
- * @package _s
+ * @package Hamburger_Cat
  */
 
 /**
@@ -14,13 +14,13 @@
  * See: https://jetpack.com/support/responsive-videos/
  * See: https://jetpack.com/support/content-options/
  */
-function _s_jetpack_setup() {
+function hamburger_cat_jetpack_setup() {
 	// Add theme support for Infinite Scroll.
 	add_theme_support(
 		'infinite-scroll',
 		array(
 			'container' => 'main',
-			'render'    => '_s_infinite_scroll_render',
+			'render'    => 'hamburger_cat_infinite_scroll_render',
 			'footer'    => 'page',
 		)
 	);
@@ -33,7 +33,7 @@ function _s_jetpack_setup() {
 		'jetpack-content-options',
 		array(
 			'post-details' => array(
-				'stylesheet' => '_s-style',
+				'stylesheet' => 'hamburger-cat-style',
 				'date'       => '.posted-on',
 				'categories' => '.cat-links',
 				'tags'       => '.tags-links',
@@ -48,20 +48,19 @@ function _s_jetpack_setup() {
 		)
 	);
 }
-add_action( 'after_setup_theme', '_s_jetpack_setup' );
+add_action( 'after_setup_theme', 'hamburger_cat_jetpack_setup' );
 
-if ( ! function_exists( '_s_infinite_scroll_render' ) ) :
-	/**
-	 * Custom render function for Infinite Scroll.
-	 */
-	function _s_infinite_scroll_render() {
-		while ( have_posts() ) {
-			the_post();
-			if ( is_search() ) :
-				get_template_part( 'template-parts/content', 'search' );
-			else :
-				get_template_part( 'template-parts/content', get_post_type() );
-			endif;
-		}
+if ( ! function_exists( 'hamburger_cat_infinite_scroll_render' ) ) :
+/**
+ * Custom render function for Infinite Scroll.
+ */
+function hamburger_cat_infinite_scroll_render() {
+	while ( have_posts() ) {
+		the_post();
+		if ( is_search() ) :
+			get_template_part( 'template-parts/content', 'search' );
+		else :
+			get_template_part( 'template-parts/content', get_post_type() );
+		endif;
 	}
 endif;
