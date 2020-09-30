aee6df0ea9dc76ab89eaba3ffb6f45294fe3fc4d
Steve <steve@australiansteve.com>
2020-09-30 14:03:36 -0300

    Remove sidebar completely

--- functions.php
+++ functions.php
@@ -118,26 +118,6 @@ function hamburger_cat_content_width() {
 add_action( 'after_setup_theme', 'hamburger_cat_content_width', 0 );
 
 /**
- * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
- */
-function hamburger_cat_widgets_init() {
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar', 'hamburger-cat' ),
-			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'hamburger-cat' ),
-			'before_widget' => '<section id="%1$s" class="widget %2$s">',
-			'after_widget'  => '</section>',
-			'before_title'  => '<h2 class="widget-title">',
-			'after_title'   => '</h2>',
-		)
-	);
-}
-add_action( 'widgets_init', 'hamburger_cat_widgets_init' );
-
-/**
  * Enqueue scripts and styles.
  */
 function hamburger_cat_scripts() {
