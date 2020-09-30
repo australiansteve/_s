927f234db8dcef7255a255448627a7ae951f5210
Steve <steve@australiansteve.com>
2020-09-30 12:34:40 -0300

    Add second menu and use Foundation Accordion for main menu

--- header.php
+++ header.php
@@ -21,16 +21,16 @@
 </head>
 
 <body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'hamburger-cat' ); ?></a>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'hamburger-cat' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
+		<header id="masthead" class="site-header">
+			<div class="site-branding">
+				<?php
+				the_custom_logo();
+				if ( is_front_page() && is_home() ) :
+					?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<?php
 			else :
@@ -46,12 +46,39 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'hamburger-cat' ); ?></button>
+			<ul class="vertical menu accordion-menu show-for-small-only" data-accordion-menu>
+				<li>
+					<a href="#"><span>Menu</span> <i class="fas fa-bars"></i><i class="fas fa-caret-up"></i></a>
+					<ul class="menu vertical nested">
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location'	=> 'menu-1',
+								'container'		=> false,
+								'items_wrap' => '%3$s'
+							)
+						);
+
+						wp_nav_menu(
+							array(
+								'theme_location'	=> 'menu-2',
+								'container'		=> false,
+								'items_wrap' => '%3$s'
+							)
+						);
+						?>
+					</ul>
+				</li>
+
+			</ul>
+
 			<?php
 			wp_nav_menu(
 				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
+					'theme_location'	=> 'menu-1',
+					'menu_id'		=> 'primary-menu',
+					'menu_class'	=> 'horizontal menu show-for-medium',
+					'container'		=> false
 				)
 			);
 			?>
