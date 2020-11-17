771f7e3d5e020e0fe6a6c68bbea94f72fc426ada
Steve <steve@australiansteve.com>
2020-11-16 22:16:08 -0400

    Living Lab and Contact page templates complete

--- page-templates/contact-page.php
+++ page-templates/contact-page.php
@@ -14,19 +14,47 @@ get_header();
 
 	<?php
 
-		$sectionId = 'landing';
-		$section = get_field($sectionId);
-		if ($section) {
-			include( locate_template( 'template-parts/section-header.php', false, false ) ); 
-			?>
-			<h1 class="page-title"><?php the_title();?></h1>
-			
+	$sectionId = 'landing';
+	$section = get_field($sectionId);
+	if ($section) {
+		include( locate_template( 'template-parts/section-header.php', false, false ) ); 
+		?>
+		<h1 class="page-title"><?php the_title();?></h1>
+
+		<div class="page-content">
+			<div class="text">
+				<?php echo $section['text'];?>
+			</div>
+			<div class="ninja-form">
+				<?php echo do_shortcode("[ninja_form id='".$section['ninja_form_id']."']"); ?>
+			</div>
+			<div class="blue-box">
+				<div class="after-form">
+					<?php echo $section['text_below_form'];?>
+				</div>
+				<div class="byline">
+					<?php echo $section['footer_byline'];?>
+					<ul>
+						<?php
+						wp_nav_menu(
+							array(
+								'theme_location'	=> 'social-menu',
+								'menu_id'		=> 'social-menu',
+								'menu_class'	=> 'horizontal menu text-left',
+								'container'		=> false
+							)
+						);
+						?>
+						
+					</div>
+				</div>
+			</div>
 			<?php
 			include( locate_template( 'template-parts/section-footer.php', false, false ) ); 
 		}
-	?>
+		?>
 
-</main><!-- #main -->
+	</main><!-- #main -->
 
-<?php
-get_footer();
+	<?php
+	get_footer();
--- page-templates/living-lab-page.php
+++ page-templates/living-lab-page.php
@@ -10,11 +10,99 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
+<main id="primary" class="site-main">
 
-		Living Lab!
+
+	<?php
+	$sectionId = 'landing';
+	$section = get_field($sectionId);
+	if ($section) {
+		include( locate_template( 'template-parts/section-header.php', false, false ) ); 
+		?>
+		<h1 class="page-title"><?php echo $section['title'];?></h1>
+		<?php
+		include( locate_template( 'template-parts/section-footer.php', false, false ) ); 
+	}
+
+	$sectionId = 'intro';
+	$section = get_field($sectionId);
+	if ($section) {
+		include( locate_template( 'template-parts/section-header.php', false, false ) ); 
+		?>
+		<div class="intro-box">
+			<?php echo $section['introductory_text']; ?>
+		</div>
+		<?php
+		include( locate_template( 'template-parts/section-footer.php', false, false ) ); 
+	}
+
+	$sectionId = 'excursions';
+	$section = get_field($sectionId);
+	if ($section) {
+		include( locate_template( 'template-parts/section-header.php', false, false ) ); 
+		?>
+		<div class="blue-box">
+			<h2><?php echo $section['subtitle']; ?></h2>
+			<div class="text">
+				<?php echo $section['text']; ?>
+			</div>
+
+			<div class="light-blue-box">
+				<div class="logo-box-title"><?php echo $section['logo_box_title']; ?></div>
+				<div class="text">
+					<?php echo $section['logo_box_text']; ?>
+				</div>
+				
+				<div id="logos" class="grid-x grid-margin-x small-up-2 medium-up-3">
+					<?php 
+					error_log("logos:".print_r($section['logos'], true));
+					foreach($section['logos'] as $logo) :
+						$link = $logo['link'];
+						?>
+						<div class='cell'>
+							<?php
+							if ($link) {
+								echo "<a class='logo-link' href='".$link."' target='blank'>";
+							}
+							?>
+							<div class="grid-y logo" style="height: 100%">
+								<div class="cell" style="flex: 1">
+									<div class="grid-y align-center image-grid" style="height: 100%">
+										<div class="cell">
+											<?php
+											$image = $logo['image'];
+											$size = 'thumbnail';
+
+											if( $image ) {
+												echo wp_get_attachment_image( $image, $size );
+											}
+											?>
+										</div>
+									</div>
+								</div>
+								<div class='cell shrink name'><?php echo $logo['name'];?></div>
+							</div>									
+							<?php
+							if ($link) {
+								echo "</a>";
+							}
+							?>
+						</div>
+						<?php
+					endforeach;
+					?>
+				</div>
+
+				<div class="text" id="byline">
+					<?php echo $section['footer_byline']; ?>
+				</div>
+			</div>
+			<?php
+			include( locate_template( 'template-parts/section-footer.php', false, false ) ); 
+		}
+		?>
 
 	</main><!-- #main -->
 
-<?php
-get_footer();
+	<?php
+	get_footer();
