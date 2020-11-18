c4d9a29632026ad155165c68b0e740c540e90e0f
Steve <steve@australiansteve.com>
2020-11-18 09:53:08 -0400

    Added buttons to Single team member pages

--- template-parts/content-austeve-team-members.php
+++ template-parts/content-austeve-team-members.php
@@ -5,8 +5,21 @@
 		<?php the_post_thumbnail('featured-image'); ?>
 		
 		<h1 class="page-title"><?php the_title();?></h1>
+
 		<?php the_content();?>
 		
+		<div class="grid-x grid-margin-x align-center buttons">
+			<?php if (get_field('email')): 
+				$hasEmail = true; ?>
+				<div class="cell medium-6 medium-text-right">
+					<a class="button" href="mailto:<?php the_field('email');?>"><?php echo the_field('contact_button_text');?></a>
+				</div>
+			<?php endif; ?>
+			<div class="cell medium-auto medium-text-<?php echo $hasEmail ? 'left' : 'center'; ?>">
+				<a class="button" href="/about"><?php echo the_field('back_to_about_button_text', 'options');?></a>
+			</div>
+		</div>
+
 		<div class="entry-footer">
 			<?php hamburger_cat_entry_footer(); ?>
 		</div><!-- .entry-footer -->
