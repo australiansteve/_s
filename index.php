742fd959eb58ea6511a6dc53d9e03629b27d6fd5
Steve <steve@australiansteve.com>
2021-02-04 21:02:35 -0400

    index.php is needed for WP installation

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+get_header();
+?>
+	<main id="primary" class="site-main">
+
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			if (has_post_thumbnail($post)) :
+				the_post_thumbnail( 'hero-image' );
+			endif;
+		?>
+
+		<div class="page-content">
+			<div class="grid-container">
+				<div class="entry-content">
+					<?php the_content(); ?>
+				</div>
+			</div>
+		</div>
+
+		<?php
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+<?php
+get_footer();
+?>
