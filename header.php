f5e033b68a71df822ee9b4886c623f9c364ac2db
Steve <steve@australiansteve.com>
2021-02-24 14:41:27 -0400

    Add option for sticky header

--- header.php
+++ header.php
@@ -12,151 +12,70 @@
 ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
-	
-</head>
+		<?php wp_head(); ?>
 
-<body <?php body_class(); ?>>
+	</head>
 
-	<?php 
-	$menuType = get_field('primary_menu_type', 'option');
+	<body <?php body_class(); ?>>
 
-	$customJS = get_field('custom_js', 'option');
+		<?php 
+		$menuType = get_field('primary_menu_type', 'option');
 
-	if( have_rows('custom_js', 'option') ):
-		while( have_rows('custom_js', 'option') ) : the_row();
-			error_log("custom_js");
+		$customJS = get_field('custom_js', 'option');
+
+		if( have_rows('custom_js', 'option') ):
+			while( have_rows('custom_js', 'option') ) : the_row();
+				error_log("custom_js");
 
 	        // Loop over sub repeater rows.
-			if( have_rows('js_script') ):
-				while( have_rows('js_script') ) : the_row();
+				if( have_rows('js_script') ):
+					while( have_rows('js_script') ) : the_row();
 
 	                // Get sub values.
-					$name = get_sub_field('name');
-					$script = get_sub_field('script');
-					$location = get_sub_field('display_in');
+						$name = get_sub_field('name');
+						$script = get_sub_field('script');
+						$location = get_sub_field('display_in');
 
-					if ($location == 'body') {
-						echo $script;
-					}
+						if ($location == 'body') {
+							echo $script;
+						}
 
-				endwhile;
-			endif;
-		endwhile;
-	endif;
-	?>
-
-	<?php wp_body_open(); ?>
-	<div id="page" class="site">
-		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'hamburger-cat' ); ?></a>
-
-		<?php
-		$useStickyHeader = get_field('use_sticky_header', 'options');
-		$stickyContainerData = $useStickyHeader ? 'data-sticky-container' : '';
-		$stickyData = $useStickyHeader ? 'data-sticky data-margin-top="0"' : '';
+					endwhile;
+				endif;
+			endwhile;
+		endif;
 		?>
-		<div <?php echo $stickyContainerData;?> >
+
+		<?php wp_body_open(); ?>
+		<div id="page" class="site">
+			<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'hamburger-cat' ); ?></a>
 
 			<?php
-			if ($menuType == 'off-canvas-top') {
-				?>
-				<div class="off-canvas-wrapper">
-					<div class="off-canvas position-top" id="offCanvasTop" data-off-canvas>
-						<div class="grid-container">
-							<div class="off-canvas-container">
-								<a class="close-off-canvas" aria-label="Close menu" type="button" data-close>
-									<i class="fas fa-times fa-2x"></i>
-								</a>
-
-								<div class="grid-y align-center">
-									<div class="cell text-center">
-										<ul class="menu vertical nested">
-											<?php
-											wp_nav_menu(
-												array(
-													'theme_location'	=> 'primary-menu',
-													'container'		=> false,
-													'items_wrap' => '%3$s'
-												)
-											);
-											?>
-											<div class="social-wrapper">
-												<?php
-												wp_nav_menu(
-													array(
-														'theme_location'	=> 'social-menu',
-														'container'		=> false,
-														'items_wrap' => '%3$s'
-													)
-												);
-												?>
-											</div>
-										</ul>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				<?php
-			} /* endif ($menuType == 'off-canvas-top') */
+			$useStickyHeader = get_field('use_sticky_header', 'options');
+			$stickyContainerData = $useStickyHeader ? 'data-sticky-container' : '';
+			$stickyData = $useStickyHeader ? 'data-sticky data-margin-top="0"' : '';
+			$stickyBgColor = get_field('sticky_header_background_color', 'options');
 			?>
+			<div <?php echo $stickyContainerData;?> >
 
-			<header id="masthead" class="site-header" <?php echo $stickyData;?>>
-				<div class="grid-container">
-
-					<?php 
-					$headerLeftClasses = ($menuType == 'top-bar') ? 'small-12 text-center' : 'medium-4 large-3 text-center medium-text-left';
-					$headerRightClasses = ($menuType == 'top-bar') ? 'small-12 text-center' : 'medium-8 large-9 medium-text-right';
+				<?php
+				if ($menuType == 'off-canvas-top') {
 					?>
-					<div class="grid-x">
-						<div class="cell <?php echo $headerLeftClasses;?> ">
-							<?php
-							$homeLink = apply_filters( 'wpml_home_url', get_option( 'home' ) );
-							?>
-							<a href="<?php echo $homeLink;?>">
-								<?php 
-								$image = get_field('header_logo', 'options');
-								$size = 'header-logo';
+					<div class="off-canvas-wrapper">
+						<div class="off-canvas position-top" id="offCanvasTop" data-off-canvas>
+							<div class="grid-container">
+								<div class="off-canvas-container">
+									<a class="close-off-canvas" aria-label="Close menu" type="button" data-close>
+										<i class="fas fa-times fa-2x"></i>
+									</a>
 
-								if( $image ) {
-									echo wp_get_attachment_image( $image, $size );
-								}
-								else {
-									echo "<h1 class='site-title'>".get_bloginfo( 'name' )."</h1>";
-								}
-								?>
-							</a>
-
-							<div id="small-language-menu" class="show-for-small-only">
-								<?php
-								if ( has_nav_menu( 'language-menu' ) ) :
-									wp_nav_menu(
-										array(
-											'theme_location'	=> 'language-menu-small',
-											'menu_id'		=> 'language-menu-small',
-											'menu_class'	=> 'horizontal menu',
-										)
-									);
-								endif;
-								?>
-							</div>
-						</div>
-						<div class="cell <?php echo $headerRightClasses;?>">
-
-							<nav id="site-navigation" class="main-navigation">
-
-								<?php
-								if ($menuType == 'top-right' || $menuType == 'top-bar') {
-									?>
-									<ul class="vertical menu accordion-menu show-for-small-only" data-accordion-menu>
-										<li>
-											<a href="#"><span>Menu</span> <i class="fas fa-bars"></i><i class="fas fa-caret-up"></i></a>
+									<div class="grid-y align-center">
+										<div class="cell text-center">
 											<ul class="menu vertical nested">
 												<?php
 												wp_nav_menu(
@@ -179,65 +98,148 @@
 													?>
 												</div>
 											</ul>
-										</li>
-
-									</ul>
-									<?php
-								} elseif ($menuType == 'off-canvas-top') {
-									?>
-									<ul class="vertical menu accordion-menu show-for-small-only" data-accordion-menu>
-										<li>
-											<a href="" class="off-canvas-top"  data-open="offCanvasTop"><span>Menu</span> <i class="fas fa-bars"></i></a>
-										</li>
-									</ul>
-									<?php
-								}
-								?>
-
-								<div id="menus">
-									<?php
-
-									if ($menuType == 'top-right') {
-										wp_nav_menu(
-											array(
-												'theme_location'	=> 'primary-menu',
-												'menu_id'		=> 'primary-menu',
-												'menu_class'	=> 'horizontal menu show-for-medium text-right',
-												'container'		=> false
-											)
-										);
-
-										if ( has_nav_menu( 'language-menu' ) ) :
-											wp_nav_menu(
-												array(
-													'theme_location'	=> 'language-menu',
-													'menu_id'		=> 'language-menu',
-													'menu_class'	=> 'vertical menu accordion-menu show-for-medium text-right',
-													'items_wrap'        => '<ul class="%2$s vertical menu accordion-menu" data-accordion-menu id="%1$s">%3$s</ul>',
-													'container' 	=> false
-												)
-											);
-										endif;
-									} elseif ($menuType == 'off-canvas-top') {
-										?>
-										<a href="" class="off-canvas-top show-for-medium" data-open="offCanvasTop"><i class="fas fa-bars fa-2x"></i></a>
-										<?php
-									} elseif ($menuType == 'top-bar') {
-										wp_nav_menu(
-											array(
-												'theme_location'	=> 'primary-menu',
-												'menu_id'		=> 'primary-menu',
-												'menu_class'	=> 'horizontal menu show-for-medium text-center',
-												'container'		=> false
-											)
-										);
-									}
-
-									?>
+										</div>
+									</div>
 								</div>
-							</nav><!-- #site-navigation -->
+							</div>
 						</div>
 					</div>
-				</div><!-- .grid-container -->
-			</header><!-- #masthead -->
-		</div>
+					<?php
+				} /* endif ($menuType == 'off-canvas-top') */
+				?>
+
+				<header id="masthead" class="site-header" <?php echo $stickyData;?> style="color:<?php echo $stickyBgColor;?>">
+					<div class="grid-container">
+
+						<?php 
+						$headerLeftClasses = ($menuType == 'top-bar') ? 'small-12 text-center' : 'medium-4 large-3 text-center medium-text-left';
+						$headerRightClasses = ($menuType == 'top-bar') ? 'small-12 text-center' : 'medium-8 large-9 medium-text-right';
+						?>
+						<div class="grid-x">
+							<div class="cell <?php echo $headerLeftClasses;?> ">
+
+								<?php
+								$homeLink = apply_filters( 'wpml_home_url', get_option( 'home' ) );
+								?>
+								<a href="<?php echo $homeLink;?>">
+									<?php 
+									$image = get_field('header_logo', 'options');
+									$size = 'header-logo';
+
+									if( $image ) {
+										echo wp_get_attachment_image( $image, $size );
+									}
+									else {
+										echo "<h1 class='site-title'>".get_bloginfo( 'name' )."</h1>";
+									}
+									?>
+								</a>
+
+								<div id="small-language-menu" class="show-for-small-only">
+									<?php
+									if ( has_nav_menu( 'language-menu' ) ) :
+										wp_nav_menu(
+											array(
+												'theme_location'	=> 'language-menu-small',
+												'menu_id'		=> 'language-menu-small',
+												'menu_class'	=> 'horizontal menu',
+											)
+										);
+									endif;
+									?>
+								</div>
+							</div>
+							<div class="cell <?php echo $headerRightClasses;?>">
+
+								<nav id="site-navigation" class="main-navigation">
+
+									<?php
+									if ($menuType == 'top-right' || $menuType == 'top-bar') {
+										?>
+										<ul class="vertical menu accordion-menu show-for-small-only" data-accordion-menu>
+											<li>
+												<a href="#"><span>Menu</span> <i class="fas fa-bars"></i><i class="fas fa-caret-up"></i></a>
+												<ul class="menu vertical nested">
+													<?php
+													wp_nav_menu(
+														array(
+															'theme_location'	=> 'primary-menu',
+															'container'		=> false,
+															'items_wrap' => '%3$s'
+														)
+													);
+													?>
+													<div class="social-wrapper">
+														<?php
+														wp_nav_menu(
+															array(
+																'theme_location'	=> 'social-menu',
+																'container'		=> false,
+																'items_wrap' => '%3$s'
+															)
+														);
+														?>
+													</div>
+												</ul>
+											</li>
+
+										</ul>
+										<?php
+									} elseif ($menuType == 'off-canvas-top') {
+										?>
+										<ul class="vertical menu accordion-menu show-for-small-only" data-accordion-menu>
+											<li>
+												<a href="" class="off-canvas-top"  data-open="offCanvasTop"><span>Menu</span> <i class="fas fa-bars"></i></a>
+											</li>
+										</ul>
+										<?php
+									}
+									?>
+
+									<div id="menus">
+										<?php
+
+										if ($menuType == 'top-right') {
+											wp_nav_menu(
+												array(
+													'theme_location'	=> 'primary-menu',
+													'menu_id'		=> 'primary-menu',
+													'menu_class'	=> 'horizontal menu show-for-medium text-right',
+													'container'		=> false
+												)
+											);
+
+											if ( has_nav_menu( 'language-menu' ) ) :
+												wp_nav_menu(
+													array(
+														'theme_location'	=> 'language-menu',
+														'menu_id'		=> 'language-menu',
+														'menu_class'	=> 'vertical menu accordion-menu show-for-medium text-right',
+														'items_wrap'        => '<ul class="%2$s vertical menu accordion-menu" data-accordion-menu id="%1$s">%3$s</ul>',
+														'container' 	=> false
+													)
+												);
+											endif;
+										} elseif ($menuType == 'off-canvas-top') {
+											?>
+											<a href="" class="off-canvas-top show-for-medium" data-open="offCanvasTop"><i class="fas fa-bars fa-2x"></i></a>
+											<?php
+										} elseif ($menuType == 'top-bar') {
+											wp_nav_menu(
+												array(
+													'theme_location'	=> 'primary-menu',
+													'menu_id'		=> 'primary-menu',
+													'menu_class'	=> 'horizontal menu show-for-medium text-center',
+													'container'		=> false
+												)
+											);
+										}
+
+										?>
+									</div>
+								</nav><!-- #site-navigation -->
+							</div>
+						</div>
+					</div><!-- .grid-container -->
+				</header><!-- #masthead -->
+			</div>
