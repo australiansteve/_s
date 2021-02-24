94c4924bb9c3651e6009c136829fc0bd07872ad2
Steve <steve@australiansteve.com>
2021-02-24 15:42:59 -0400

    Allow customization of taxonomy term description font size

--- template-parts/archive-description-austeve-projects.php
+++ template-parts/archive-description-austeve-projects.php
@@ -1,6 +1,9 @@
 
 <?php if (is_tax('project-category')) {
-	the_archive_description( '<div class="archive-description">', '</div>' );
+	$term = get_queried_object();
+	$fontSize = get_field('archive_description_font_size', $term);
+
+	the_archive_description( '<div class="archive-description" style="font-size: '.$fontSize.'px">', '</div>' );
 }
 else { 
 	/* Top level archive page - read from settings rather than from CPT definition */
