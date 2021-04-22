2490c1875f8107cc6247b34afacb8a17ffb7780f
Steve <steve@australiansteve.com>
2021-04-22 15:26:16 -0300

    Add some missing functions in functions.php

--- functions.php
+++ functions.php
@@ -240,8 +240,36 @@ add_filter ( 'pre_get_posts', function($query) {
 		
 		/* Always get 'all' funds ie up to 200 */
 	    $query->set( 'posts_per_page', '200' );
-	    
-		return $query;        
+	    $query->set('meta_key', 'stripped_name');
+		$query->set('orderby', 'meta_value');
+		$query->set('order'	, 'ASC');
+
+		if(isset($_GET['fund-name']))
+		{
+			$query->set('s', $_GET['fund-name']);
+			//error_log(print_r($query, true));
+		}
+		if(isset($_GET['fund-category']))
+		{
+			//error_log(print_r($query, true));
+			$tax_query = array(
+				'taxonomy' => 'austeve-funds-category',
+				'field'    => 'slug',
+				'terms' => explode(",", $_GET['fund-category']),
+				'operator' => 'IN',
+				'include_children' => true
+			);
+
+			$query->set( 'tax_query', array(
+				'relation' => 'AND',
+				$tax_query
+			));
+
+			//error_log(print_r($query, true));
+		}
+		//error_log(print_r($query, true));
+
+		return $query;      
 	}
 }, 10, 1);
 
@@ -275,3 +303,119 @@ add_filter( 'get_the_archive_title', function ($title) {
 	}
 	return $title;
 });
+
+function austeve_populate_fund_selector_values($field) {
+
+	//Only get list of funds when admin page (ie. A fund) is being edited in the backend
+	if (is_admin())
+	{
+		// reset choices
+		$field['choices'] = array();
+
+		// get the textarea value from options page without any formatting
+		$url = get_field('canada_helps_webservice_url', 'option');
+
+		//$args = array( 'timeout' => 120, 'httpversion' => '1.1' );
+		$response = wp_remote_get( $url );
+		//error_log("Canada Helps page response: ".print_r($response, true));
+
+		$field['choices']["NO_FUND"] = "Select fund";
+
+		if ( is_array( $response ) ) {
+			$header = $response['headers']; // array of http header lines
+			$body = $response['body']; // use the content
+
+			$jsonResponse = json_decode($body);
+
+			if ($jsonResponse->CharityFunds)
+			{
+				$funds = $jsonResponse->CharityFunds;
+
+				if (count($funds) == 0)
+				{
+					$field['choices']["ERROR03"] = "ERROR03: No funds could be found in the response from CanadaHelps. Reload page to try again. Contact steve@weavercrawford.com if the problem persists";
+
+				}
+
+				foreach($funds as $fund)
+				{
+					//error_log("Fund: ".print_r($fund, true));
+			 		$field['choices'][$fund->FundID] = $fund->FundDescription;
+				}
+			}
+			else
+			{
+				$field['choices']["ERROR02"] = "ERROR02: Could not get Funds from CanadaHelps. Reload page to try again. Contact steve@weavercrawford.com if the problem persists";
+			}
+		}
+		else
+		{
+			$field['choices']["ERROR01"] = "ERROR01: Could not retrieve response from CanadaHelps. Reload page to try again";
+		}
+
+		//error_log("Choices: ".print_r($field['choices'], true));
+	}
+
+	return $field;
+}
+add_filter('acf/load_field/name=canada_helps_fund_id', 'austeve_populate_fund_selector_values');
+add_filter('acf/load_field/name=canada_helps_default_fund', 'austeve_populate_fund_selector_values');
+
+function austeve_populate_color_selector_values($field) {
+
+	//Only get list of funds when admin page is being edited in the backend
+	if (is_admin())
+	{
+		// reset choices
+		$field['choices'] = array(
+			'#bc5298' => 'Pink',
+			'#7fb955' => 'Green',
+			'#e68f52' => 'Orange',
+			'#e4e164' => 'Yellow',
+			'#6abfdb' => 'Blue',
+			'#6cc2dd' => 'Bright Blue',
+		);
+	}
+
+	return $field;
+}
+
+add_filter('acf/load_field/name=color', 'austeve_populate_color_selector_values');
+add_filter('acf/load_field/name=featured_post_background_color', 'austeve_populate_color_selector_values');
+add_filter('acf/load_field/name=default_fund_background_color', 'austeve_populate_color_selector_values');
+add_filter('acf/load_field/name=about_sub_page_link_color', 'austeve_populate_color_selector_values');
+add_filter('acf/load_field/name=grant_highlight_color', 'austeve_populate_color_selector_values');
+
+function austeve_get_stripped_fund_name($fundName)
+{
+	$strippedName = $fundName;
+	error_log(strpos(strtolower($fundName), 'the '));
+	if (strpos(strtolower($fundName), 'the ') === 0)
+	{
+		error_log("Fund name starts with 'the'");
+		$strippedName = substr($fundName, 4);
+	}
+	error_log('Updating stripped_name attribute of fund: '.$strippedName);
+	return $strippedName;
+}
+
+function austeve_set_post_stripped_name($post_id) {
+	$post_type = get_post_type($post_id);
+	if ($post_type != 'austeve-funds') {
+		return;
+	}
+	$post = get_post($post_id);
+	error_log('Fund has been saved: '.$post->post_title);
+	$strippedName = austeve_get_stripped_fund_name($post->post_title);
+	update_field('stripped_name', $strippedName, $post_id);
+}
+
+add_action('acf/save_post', 'austeve_set_post_stripped_name', 20 );
+
+
+function austeve_posts_link_attributes() {
+    return 'class="button"';
+}
+add_filter('next_posts_link_attributes', 'austeve_posts_link_attributes');
+add_filter('previous_posts_link_attributes', 'austeve_posts_link_attributes');
+
