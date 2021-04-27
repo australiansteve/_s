03ff7e680a5af1e3a28e8d9dee8c2253bb51fee9
Steve <steve@australiansteve.com>
2021-04-27 15:17:50 -0300

    Improve fund search capacity on the Funds page, and load less funds by default (use AJAX to display more after loading)

--- archive-austeve-funds.php
+++ archive-austeve-funds.php
@@ -5,6 +5,8 @@
 
 get_header(); 
 
+$ajax_nonce = wp_create_nonce( "archive-page-posts" );
+
 global $categoryBgColors; 
 $savedBgColors = get_field('fund_category_background_colors', 'options');
 foreach($savedBgColors as $color)
@@ -38,7 +40,7 @@ foreach($savedBgColors as $color)
 
 							<div class="cell small-12 medium-3" id="filter-sidebar" data-sticky-container>
 
-								<div class="sticky" data-sticky data-top-anchor="page-introduction:bottom" data-anchor="filter-results" data-margin-top="9">
+								<div class="sticky" data-sticky data-top-anchor="page-introduction:bottom" data-anchor="filter-results" data-margin-top="10">
 
 									<form method="GET" action="#" id="search-filters" class="search-form" onsubmit="return validateSearch()">
 										<input type="hidden" name="austeve-funds-archive" value="true"/>
@@ -52,8 +54,9 @@ foreach($savedBgColors as $color)
 										</div>
 										<div class="grid-x" id="fund-category-filters">
 											<?php
-											$excludeCategories[] = get_term_by( 'slug', 'featured', 'austeve-funds-category' )->term_id;
 
+											/* Find all terms except for 'featured' */
+											$excludeCategories[] = get_term_by( 'slug', 'featured', 'austeve-funds-category' )->term_id;
 											$terms = get_terms( 'austeve-funds-category', array(
 												'hide_empty' => false,
 												'exclude' => $excludeCategories,
@@ -120,7 +123,6 @@ foreach($savedBgColors as $color)
 									</div>
 									<div class="placeholder">&nbsp;</div> <!-- Needed to stop the bottom margin of the #donate-now being lost when equalizer does it's thing on small screens -->
 
-
 								</div>
 
 							</div>
@@ -135,81 +137,135 @@ foreach($savedBgColors as $color)
 										while ( have_posts() ) : the_post();
 											get_template_part('template-parts/austeve-funds', 'archive');
 										endwhile; 
-										else: ?>
-											<div class="cell fund auto" class="fund-title">
-												No funds could be found with your search criteria. Please try a broader search or view <a href='/funds'>all funds</a>
-											</div>
-										<?php endif; ?>
-
-									</div>
-
+										?>
+										<div class="cell text-center loading-more-funds">
+											<i class="fas fa-spinner fa-spin" style="padding: 2rem;"></i>
+										</div>
+										<?php
+									else: ?>
+										<div class="cell fund auto" class="fund-title">
+											No funds could be found with your search criteria. Please try a broader search or view <a href='/funds'>all funds</a>
+										</div>
+									<?php 
+									endif;
+									?>
 								</div>
 
 							</div>
 
 						</div>
 
-						<?php
-						global $wp;
-						$home_url = home_url();
-						$current_url = home_url(add_query_arg(array(),$wp->request));
-						$afterhome = strlen($current_url) - strlen($home_url);
-						$request_url = substr($current_url, - ($afterhome-1));
-						?>
+					</div>
 
-						<script type="text/javascript">
-							jQuery("input[name='fund-category']").on('click', function(){
-								validateSearch(jQuery(this).val() === 'all-funds');
+					<?php
+					global $wp;
+					$home_url = home_url();
+					$current_url = home_url(add_query_arg(array(),$wp->request));
+					$afterhome = strlen($current_url) - strlen($home_url);
+					$request_url = substr($current_url, - ($afterhome-1));
+					?>
+
+					<script type="text/javascript">
+						jQuery("input[name='fund-category']").on('click', function(){
+							validateSearch(jQuery(this).val() === 'all-funds');
+						});
+
+						function validateSearch(clearCategoryFilter) {
+							var url = "<?php echo home_url( $request_url ); ?>";
+							var args = {};
+
+							jQuery('#search-filters .filter').each(function(){
+
+								var filter = jQuery(this).data('filter'),
+								vals = [ jQuery(this).val()];
+
+								args[ filter ] = vals.join(',');
+
 							});
 
-							function validateSearch(clearCategoryFilter) {
-								var url = "<?php echo home_url( $request_url ); ?>";
-								var args = {};
-
-								jQuery('#search-filters .filter').each(function(){
-
-									var filter = jQuery(this).data('filter'),
-									vals = [ jQuery(this).val()];
-
-									args[ filter ] = vals.join(',');
-
-								});
-
-								var diffVals = [];
-								jQuery('#search-filters input[name=fund-category]').each(function(){
-									if (jQuery(this).attr('checked') == 'checked' && jQuery(this).val() != 'all-funds')
-									{
-										diffVals.push(jQuery(this).val());
-									}
-								});
-								args[ 'fund-category' ] = diffVals.join(',');
-
-								/*If 'all funds' option has been selected clear the array */
-								if (clearCategoryFilter)
+							var diffVals = [];
+							jQuery('#search-filters input[name=fund-category]').each(function(){
+								if (jQuery(this).prop('checked') && jQuery(this).val() != 'all-funds')
 								{
-									args = {};
+									diffVals.push(jQuery(this).val());
 								}
-								/* update url */
-								url += '?';
+							});
+							args[ 'fund-category' ] = diffVals.join(',');
 
-								jQuery.each(args, function( name, value ){	
-									if (value.length > 0)		
-										url += name + '=' + value + '&';			
-								});
-
-								/* remove last & */
-								url = url.slice(0, -1);
-								/* reload page */
-								window.location.replace( url );
-								return false;
+							/*If 'all funds' option has been selected clear the array */
+							if (clearCategoryFilter)
+							{
+								args = {};
 							}
-						</script>
 
-					</div>
+							/* update url */
+							url += '?';
+
+							jQuery.each(args, function( name, value ){	
+								if (value.length > 0)		
+									url += name + '=' + value + '&';			
+							});
+
+							/* remove last & */
+							url = url.slice(0, -1);
+							
+							/* reload page */
+							window.location.replace( url );
+							return false;
+						}
+
+						function loadMoreFunds(pageNumber) {
+							var diffVals = [];
+							jQuery('#search-filters input[name=fund-category]').each(function(){
+								if (jQuery(this).prop('checked') && jQuery(this).val() != 'all-funds')
+								{
+									diffVals.push(jQuery(this).val());
+								}
+							});
+							var category = diffVals.join(',');
+
+							var search = jQuery('#search-filters .filter').val();
+
+							jQuery.ajax({
+					 			type: 'POST',
+					 			url: '<?php echo admin_url('admin-ajax.php');?>',
+					 			dataType: "html",  
+					 			data: { 
+					 				action : 'austeve_get_funds', 
+					 				security: '<?php echo $ajax_nonce; ?>', 
+					 				page: pageNumber, 
+					 				category: category, 
+					 				s: search
+					 			},
+					 			error: function (xhr, status, error) {
+					 				console.log("Error: " + error);
+					 				
+					 			},
+					 			success: function( response ) {
+					 				if (response) {
+					 					//console.log(response);
+					 					jQuery( '#filter-results .loading-more-funds' ).before( response );
+					 					window.dispatchEvent(new Event('resize'));
+					 					loadMoreFunds(pageNumber + 1);
+					 				}
+					 				else {
+					 					/* No more responses */					
+					 					jQuery( '#filter-results .loading-more-funds' ).remove();
+					 				}
+					 			}
+					 		});
+						}
+
+						jQuery(document).ready(function() {
+							console.log("Load next page");
+							loadMoreFunds(2);
+						});
+					</script>
 
 				</div>
 			</div>
 		</div>
-	</main><!-- #main -->
+	</div>
+</main><!-- #main -->
 
 	<?php get_footer(); ?>
