614ec42aa07dd7758e108ed3298dc6cf05d19e63
Steve <steve@australiansteve.com>
2021-05-05 20:20:58 -0300

    Add option to do off canvas menu that drops down from top

--- js/foundation.js
+++ js/foundation.js
@@ -2,12 +2,14 @@ import { Foundation } from 'foundation-sites/js/foundation.core'
 import { AccordionMenu } from 'foundation-sites/js/foundation.accordionMenu'
 import { Sticky } from 'foundation-sites/js/foundation.sticky'
 import { Equalizer } from 'foundation-sites/js/foundation.equalizer'
+import { OffCanvas } from 'foundation-sites/js/foundation.offcanvas'
 
 function initFoundation() {
   // Include the Foundation Modules that we'll use in the app
   Foundation.plugin(AccordionMenu, 'AccordionMenu')
   Foundation.plugin(Sticky, 'Sticky')
   Foundation.plugin(Equalizer, 'Equalizer')
+  Foundation.plugin(OffCanvas, 'OffCanvas')
   
   // Add jQuery to Foundation
   Foundation.addToJquery(jQuery)
