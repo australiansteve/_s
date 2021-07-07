237ca0205b0e401807db6b561270fe1449dccb8b
Steve <steve@australiansteve.com>
2021-07-07 13:09:55 -0300

    Add hero image bling and a special case for the home page (top-centered)

--- template-parts/hero-image.php
+++ template-parts/hero-image.php
@@ -3,14 +3,18 @@ $displayHeroImage = get_field('display_hero_image');
 $alternateHeroImageId = get_field('use_alternative_hero_image');
 
 if ($displayHeroImage) :
-	
+	?>
+	<div class="hero-container bling container">
+		<?php
 	if($alternateHeroImageId) {
 		echo wp_get_attachment_image( $alternateHeroImageId, 'hero-image' );
 	}
 	else if (has_post_thumbnail($post)) {
 		the_post_thumbnail( 'hero-image' );
 	}
-
+?>
+	</div>
+		<?php
 endif;
 
 ?>
