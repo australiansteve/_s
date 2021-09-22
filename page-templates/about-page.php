8028cd99ea2d326e3ee923d0d6b5e505ce3b4716
Steve <steve@australiansteve.com>
2021-09-22 14:26:53 -0300

    Ready for launch

--- page-templates/about-page.php
+++ page-templates/about-page.php
@@ -40,44 +40,50 @@
  					</div>
  				</section>
 
- 				<section id="section-2">
- 					<div class="entry-content no-padding" id="section-2-content">
- 						<div class="iframe-container">
- 							<iframe class="responsive" src="https://player.vimeo.com/video/<?php the_field('section_2_video_id');?>?color=c02c8b&title=0&byline=0&portrait=0&autoplay=0&loop=0&autopause=0&muted=0&controls=1&background=0" frameborder="0"allow="autoplay; fullscreen; picture-in-picture" muted autoplay></iframe>
- 						</div>
- 					</div>
- 				</section>
+                <?php 
+                $video_id = get_field('section_2_video_id');
 
- 				<section id="section-3">
- 					<div class="entry-content">
- 						<h2 class="page-title"><span><?php the_field('section_3_title');?></span></h2>
- 						<div class="grid-x">
- 							<div class="cell medium-6">
- 								<?php 
- 								$image = get_field('section_3_image');
- 								$size = 'about-portrait'; 
+                if ($video_id) :
+                    ?>
+                    <section id="section-2">
+                        <div class="entry-content no-padding" id="section-2-content">
+                            <div class="iframe-container">
+                                <iframe class="responsive" src="https://player.vimeo.com/video/<?php echo $video_id;?>?color=c02c8b&title=0&byline=0&portrait=0&autoplay=0&loop=0&autopause=0&muted=0&controls=1&background=0" frameborder="0"allow="autoplay; fullscreen; picture-in-picture" muted autoplay></iframe>
+                            </div>
+                        </div>
+                    </section>
+                    <?php
+                endif;
+                ?>
+                <section id="section-3">
+                    <div class="entry-content">
+                        <h2 class="page-title"><span><?php the_field('section_3_title');?></span></h2>
+                        <div class="grid-x">
+                            <div class="cell medium-6">
+                                <?php 
+                                $image = get_field('section_3_image');
+                                $size = 'about-portrait'; 
 
- 								if( $image ) {
- 									echo wp_get_attachment_image( $image, $size );
- 								}
- 								?>
- 							</div>
- 							<div class="cell medium-6" id="section-3-text">
- 								<?php the_field('section_3_text');?>
- 							</div>
- 						</div>
- 					</div>
- 				</section>
+                                if( $image ) {
+                                  echo wp_get_attachment_image( $image, $size );
+                              }
+                              ?>
+                            </div>
+                            <div class="cell medium-6" id="section-3-text">
+                                <?php the_field('section_3_text');?>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div> 
+        </div>
 
- 			</div> 
- 		</div>
+    <?php
 
- 		<?php
+endwhile;
+?>
 
- 	endwhile;
- 	?>
+</main><!-- #main -->
 
- </main><!-- #main -->
-
- <?php
- get_footer();
+<?php
+get_footer();
