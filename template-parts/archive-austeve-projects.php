41b29f820e24f0b0eb1a91133d7c480a61c49451
Steve <steve@australiansteve.com>
2021-12-30 21:28:34 -0400

    Remove references to Projects CPT

--- template-parts/archive-austeve-projects.php
+++
@@ -1,7 +0,0 @@
-<li>
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<a href="<?php echo the_permalink();?>">
-			<p class="entry-title"><?php the_title();?></p>
-		</a>
-	</article><!-- #post-<?php the_ID(); ?> -->
-</li>
