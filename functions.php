b26e4a69c596c5acafd1690cdf01d8ef4d8680ca
Steve <steve@australiansteve.com>
2022-09-20 08:57:15 -0300

    Customizae default wordpress login page

--- functions.php
+++ functions.php
@@ -285,3 +285,25 @@ add_action('wp_footer', function() {
 		echo get_template_part('template-parts/js-ajax-add-to-cart');
 	}
 });
+
+
+add_action( 'login_enqueue_scripts', function() { 
+	?>
+    <style type="text/css">
+        #login h1 a, .login h1 a {
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/media/inky-logo-dark.png);
+			height:197px;
+			width:240px;
+			background-size: 240px 197px;
+			background-repeat: no-repeat;
+        	padding-bottom: 30px;
+        }
+    </style>
+	<?php 
+});
+
+ 
+add_action( 'login_enqueue_scripts', function() {
+    wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/style-login.css' );
+    wp_enqueue_script( 'custom-login', get_stylesheet_directory_uri() . '/style-login.js' );
+} );
