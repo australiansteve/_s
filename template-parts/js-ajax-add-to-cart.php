b6e00b279a617c255777a280a3e8576fa2e88370
Steve Weaver Crawford <steve@australiansteve.com>
2023-10-02 21:50:54 -0300

    handle item already being added to cart

--- template-parts/js-ajax-add-to-cart.php
+++ template-parts/js-ajax-add-to-cart.php
@@ -5,6 +5,7 @@ $ajax_nonce_wishlist_update = wp_create_nonce( "update-wishlist" );
 ?>
 <script type="text/javascript">
 	function add_to_cart(target, product_id, variation_id, campaign_id, wishlist_id) {
+        var initial_cart_count = jQuery('header span.header-cart-count').html();
 
 		jQuery('header span.header-cart-count').html("<i class='fas fa-circle-notch fa-spin'></i>");
 		target.append("<i class='fas fa-circle-notch fa-spin button-spinner'></i>");
@@ -32,10 +33,17 @@ $ajax_nonce_wishlist_update = wp_create_nonce( "update-wishlist" );
                 if (response) {
                 	console.log('austeve_add_to_cart response: ' + response);
             		target.find('.button-spinner').remove();
-            		target.after("<div class='message add-to-cart-response'><?php _e('Added to cart.', 'hamburger-cat'); ?><br/><?php echo sprintf(__('<a href=\'%s\'><strong>View Cart</strong>.</a>', 'hamburger-cat'), wc_get_cart_url()); ?></div>");
-                    setTimeout(function() {
-                    	jQuery('header span.header-cart-count').html(response);
-                    }, 1500);                    
+                    if (response == 'wishlist-item-already-purchased') {
+                        target.after("<div class='message add-to-cart-response'><?php _e('Wishlist item already purchased or found in cart', 'hamburger-cat'); ?></div>");
+                        jQuery('header span.header-cart-count').html(initial_cart_count);
+
+                    }
+                    else {
+                		target.after("<div class='message add-to-cart-response'><?php _e('Added to cart.', 'hamburger-cat'); ?><br/><?php echo sprintf(__('<a href=\'%s\'><strong>View Cart</strong>.</a>', 'hamburger-cat'), wc_get_cart_url()); ?></div>");
+                        setTimeout(function() {
+                        	jQuery('header span.header-cart-count').html(response);
+                        }, 1500);
+                    }
                 }
                 else {
             		target.find('.button-spinner').remove();
